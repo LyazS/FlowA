4c92b1ced0dcea1e77ed2ba19a41d98d34e8ec7a
airzostorm <airzostorm@outlook.com>
2025-03-11 23:52:30 +0800

    docs: 更新BaseComponent类Slots属性的描述和类型

--- backend/app/uisdk/baseComponent.py
+++ backend/app/uisdk/baseComponent.py
@@ -1,9 +1,16 @@
 from typing import List, Dict
 from pydantic import BaseModel
 
+"""
+Type 指向组件的类型，例如NInput，NFlex等
+Props 字典，包含组件的属性，仅限于基础属性，不能包含事件属性
+    对于v-model，可以直接在Props中定义，将自动设置事件
+Slots 字典，可以包含子组件，将会设置()=>{}的事件
+"""
 
 class BaseComponent(BaseModel):
     Type: str
     Props: Dict
-    Slots: List
+    Slots: Dict
     pass
+
