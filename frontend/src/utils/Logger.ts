160b5806495ac28d3de1043aa8bdd87cf95a34d6
AirzoStorm <ly1559909891@live.com>
2025-03-15 07:58:19 +0000

    feat(utils): 添加logger库

---
+++ frontend/src/utils/Logger.ts
@@ -0,0 +1,25 @@
+class Logger {
+  private name: string
+
+  constructor(name: string) {
+    this.name = name
+  }
+
+  log(...data: any[]): void {
+    console.log(`[${this.name}]`, ...data)
+  }
+
+  info(...data: any[]): void {
+    console.info(`[${this.name}]`, ...data)
+  }
+
+  warn(...data: any[]): void {
+    console.warn(`[${this.name}]`, ...data)
+  }
+
+  error(...data: any[]): void {
+    console.error(`[${this.name}]`, ...data)
+  }
+}
+
+export default Logger
