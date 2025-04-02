f0519dd961baa21c66950a456806314317b010c8
zhuowenhao <zhuowenhao@huya.com>
2025-04-02 10:10:30 +0800

    refactor: 重构UI_TagOutput组件

--- backend/plugins/builtin/UI_Components/UI_TagOutput.py
+++ backend/plugins/builtin/UI_Components/UI_TagOutput.py
@@ -5,6 +5,134 @@ from .RefVarSelect import UI_RefVarSelect
 from app.schemas.VFNodeInterface import VFNodeConnectionType
 
 
+class handleTag(NormalComponent):
+    def __init__(self):
+        super().__init__(
+            Type="NDivider",
+            Props={
+                "title-placement": "left",
+                "style": {
+                    "width": "75%",
+                    "margin": 0,
+                },
+            },
+            Slots={
+                "default": NormalComponent(
+                    Type="NText",
+                    Props={
+                        "type": "warning",
+                        "strong": True,
+                    },
+                    Slots={
+                        "default": SpanComponent(
+                            Type=ComponentType.VBIND,
+                            Data=[
+                                VFOR_DATA,
+                                "@ConnectHandles",
+                                "Label",
+                            ],
+                        )
+                    },
+                ),
+            },
+        )
+
+
+class outputTag(NFlex):
+    def __init__(self):
+        super().__init__(
+            vertical=True,
+            slots={
+                "default": [
+                    ForLoopComponent(
+                        Items=VBindProp(
+                            Data=[
+                                VFOR_DATA,
+                                "@ConnectHandles",
+                                "Data",
+                            ]
+                        ),
+                        ItemLabel="@ConnectItems",
+                        IndexLabel="@ItemIndex",
+                        Template=NFlex(
+                            vertical=False,
+                            wrap=False,
+                            justify="flex-start",
+                            style={
+                                "align-content": "center",
+                                "align-items": "center",
+                                "width": "50%",
+                                "margin": 0,
+                                "padding-left": "50px",
+                            },
+                            slots={
+                                "default": [
+                                    NText(
+                                        depth=2,
+                                        slots={
+                                            "default": SpanComponent(
+                                                Type=ComponentType.VBIND,
+                                                Data=[
+                                                    VFOR_DATA,
+                                                    "@ConnectItems",
+                                                    "DataLabel",
+                                                ],
+                                            ),
+                                        },
+                                    ),
+                                    NText(
+                                        depth=2,
+                                        slots={
+                                            "default": SpanComponent(
+                                                Type=ComponentType.VALUE,
+                                                Data=" - ",
+                                            ),
+                                        },
+                                    ),
+                                    NText(
+                                        depth=2,
+                                        type="info",
+                                        italic=True,
+                                        slots={
+                                            "default": SpanComponent(
+                                                Type=ComponentType.VBIND,
+                                                Data=[
+                                                    VFOR_DATA,
+                                                    "@ConnectItems",
+                                                    "DataType",
+                                                ],
+                                            ),
+                                        },
+                                    ),
+                                ],
+                            },
+                        ),
+                    ),
+                ]
+            },
+        )
+
+
+class outputCard(NormalComponent):
+    def __init__(self):
+        super().__init__(
+            Type="NCard",
+            Props={
+                "bordered": True,
+                "hoverable": True,
+                "size": "small",
+                "style": {
+                    "width": "100%",
+                    "margin-bottom": "10px",
+                },
+            },
+            Slots={
+                "header": handleTag(),
+                "default": outputTag(),
+            },
+        )
+
+
 class UI_TAG_OUTPUTS(NFlex):
     def __init__(self):
         super().__init__(
@@ -24,107 +152,7 @@ class UI_TAG_OUTPUTS(NFlex):
                         Template=NFlex(
                             vertical=True,
                             slots={
-                                "default": [
-                                    NormalComponent(
-                                        Type="NDivider",
-                                        Props={
-                                            "title-placement": "left",
-                                            "style": {
-                                                "width": "75%",
-                                                "margin": 0,
-                                            },
-                                        },
-                                        Slots={
-                                            "default": NormalComponent(
-                                                Type="NText",
-                                                Props={
-                                                    "type": "warning",
-                                                    "strong": True,
-                                                },
-                                                Slots={
-                                                    "default": SpanComponent(
-                                                        Type=ComponentType.VBIND,
-                                                        Data=[
-                                                            VFOR_DATA,
-                                                            "@ConnectHandles",
-                                                            "Label",
-                                                        ],
-                                                    )
-                                                },
-                                            ),
-                                        },
-                                    ),
-                                    NFlex(
-                                        vertical=True,
-                                        slots={
-                                            "default": [
-                                                ForLoopComponent(
-                                                    Items=VBindProp(
-                                                        Data=[
-                                                            VFOR_DATA,
-                                                            "@ConnectHandles",
-                                                            "Data",
-                                                        ]
-                                                    ),
-                                                    ItemLabel="@ConnectItems",
-                                                    IndexLabel="@ItemIndex",
-                                                    Template=NormalComponent(
-                                                        Type="NDivider",
-                                                        Props={
-                                                            "title-placement": "left",
-                                                            "style": {
-                                                                "width": "50%",
-                                                                "margin": 0,
-                                                                "padding-left": "20px",
-                                                            },
-                                                        },
-                                                        Slots={
-                                                            "default": [
-                                                                NText(
-                                                                    depth=2,
-                                                                    slots={
-                                                                        "default": SpanComponent(
-                                                                            Type=ComponentType.VBIND,
-                                                                            Data=[
-                                                                                VFOR_DATA,
-                                                                                "@ConnectItems",
-                                                                                "DataLabel",
-                                                                            ],
-                                                                        ),
-                                                                    },
-                                                                ),
-                                                                NText(
-                                                                    depth=2,
-                                                                    slots={
-                                                                        "default": SpanComponent(
-                                                                            Type=ComponentType.VALUE,
-                                                                            Data=" - ",
-                                                                        ),
-                                                                    },
-                                                                ),
-                                                                NText(
-                                                                    depth=2,
-                                                                    type="info",
-                                                                    italic=True,
-                                                                    slots={
-                                                                        "default": SpanComponent(
-                                                                            Type=ComponentType.VBIND,
-                                                                            Data=[
-                                                                                VFOR_DATA,
-                                                                                "@ConnectItems",
-                                                                                "DataType",
-                                                                            ],
-                                                                        ),
-                                                                    },
-                                                                ),
-                                                            ],
-                                                        },
-                                                    ),
-                                                ),
-                                            ]
-                                        },
-                                    ),
-                                ]
+                                "default": outputCard(),
                             },
                         ),
                     ),
